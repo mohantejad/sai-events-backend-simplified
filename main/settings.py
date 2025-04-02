ce06060ac49de21705096412b66a770e52eb4848
Mohanteja Dharmavarapu <mohantejadharmavarapu@Mohantejas-MacBook-Pro.local>
2025-04-02 16:20:56 +0530

    Updated staticfiles for lightsail

--- main/settings.py
+++ main/settings.py
@@ -91,6 +91,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
