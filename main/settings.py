d94fac5e067757c57bc7ca4aca1d0ce1a9d1066c
Mohanteja Dharmavarapu <mohantejadharmavarapu@Mohantejas-MacBook-Pro.local>
2025-04-02 20:20:10 +0530

    added from docs

--- main/settings.py
+++ main/settings.py
@@ -93,7 +93,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
